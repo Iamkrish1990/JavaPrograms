5940a8cc24e8d22f8e5791a6565800adc0627db2
Krishnendu <krishnendunandi1990@gmail.com>
2020-04-20 23:39:09 +0530

    sample

---
+++ src/oops/inheritance/ClassA.java
@@ -0,0 +1,14 @@
+package oops.inheritance;
+
+public class ClassA {
+	
+	public void test1() {
+		
+		System.out.println("In Super class");
+	}
+	
+	public void test2() {
+		
+	}
+
+}
---
+++ src/oops/inheritance/ClassB.java
@@ -0,0 +1,13 @@
+package oops.inheritance;
+
+public class ClassB extends ClassA{
+
+	public static void main(String[] args) {
+		
+		ClassB b = new ClassB();
+		b.test1();
+		
+
+	}
+
+}
